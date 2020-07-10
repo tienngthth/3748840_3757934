563082e1e48079b1dda6303da1a514a50abd2c44
Tien <s3757934@rmit.edu.vn>
2020-07-10 14:08:05 +0700

    commit

--- animatedEmoji.py
+++ animatedEmoji.py
@@ -4,23 +4,25 @@ from time import sleep
 sense = SenseHat()
 
 # Define some colours
-g = (0, 255, 0) # Green
-r = (255, 0, 0) # Red
-bl = (0, 0, 255) # Blue
-b = (0, 0, 0) # Black
+lo = (255, 165, 0) # Light Orange
+br = (165, 42, 42) # Brown
+db = (0, 0, 255) # Dark Blue
+lb = (0, 102, 255) # Light Blue
+bl = (0, 0, 0) # Black
+cr = (255, 255, 179) # Cream
 
 class Emoji:
   def __init__(self, order):
     if order == 1:
         self.image = [
-            g, g, g, g, g, g, g, g,
-            g, g, g, g, g, g, g, g,
-            g, b, b, g, g, b, b, g,
-            g, b, b, g, g, b, b, g,
-            g, g, g, b, b, g, g, g,
-            g, g, b, b, b, b, g, g,
-            g, g, b, b, b, b, g, g,
-            g, g, b, g, g, b, g, g
+            bl, lo, bl, bl, bl, bl, br, bl,
+            bl, lo, br, bl, bl, br, cr, bl,
+            bl, lo, lo, br, lb, cr, cr, bl,
+            lb, lb, lo, lb, lo, lb, lo, bl,
+            lo, bl, lo, lo, bl, lo, lo, bl,
+            lo, bl, lo, lo, bl, lo, lo, lo,
+            br, lo, cr, lo, lo, lo, lo, bl,
+            bl, br, bl, lo, lo, br, bl, bl
         ]
     elif order == 2:
         self.image = [
@@ -45,12 +47,14 @@ class Emoji:
             g, g, b, g, g, b, g, g
         ]
 
-  def displayEmoji(self):
+  def display(self):
     sense.set_pixels(self.image)
 
-emojis = [Emoji(1), Emoji(2), Emoji(3)]
+# emojis = [Emoji(1), Emoji(2), Emoji(3)]
+emoji = Emoji(1)
+emoji.display()
 
-while True:
-    for emoji in emojis:
-        emoji.displayEmoji()
-        sleep(3)
+# while True:
+#     for emoji in emojis:
+#         emoji.display()
+#         sleep(3)
