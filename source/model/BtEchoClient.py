aec7d870ff4ba07147002e52db52853a9f7e4bbf
tienngthth <s3757934@rmit.edu.vn>
2020-07-29 12:22:47 +0700

    bluetooth

---
+++ source/model/BtEchoClient.py
@@ -0,0 +1,12 @@
+import bluetooth
+
+bd_addr = "DC:A6:32:4A:0C:41"
+
+port = 1
+
+sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+sock.connect((bd_addr, port))
+
+sock.send("hello!!")
+
+sock.close()
