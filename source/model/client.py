e5f86cc5b87baac3435f80fcfc3903ea7ae21b95
tienngthth <s3757934@rmit.edu.vn>
2020-07-29 17:37:06 +0700

    Two way bluetooth

--- source/model/client.py
+++ source/model/client.py
@@ -1,14 +1,36 @@
+# import bluetooth
+
+# class Client:
+#     sock=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+
+#     def __init__(bluetooth_address, port):
+#         self.port = port
+#         self.bluetooth_address = bluetooth_address
+
+#     def open_socket():
+#         sock.connect((bd_addr, port))
+
+#     def send_message(message)
+#         sock.send(message)
+
+#     def close_socke():
+#         sock.close()
+
 import bluetooth
 
-class Client:
+bd_addr = "DC:A6:32:4A:0C:41"
 
-    bd_addr = "DC:A6:32:4A:0C:41"
+client_port = 1
+server_port =2
+server_sock.bind(("",server_port))
+server_sock.listen(1)
 
-    port = 1
+sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+sock.connect((bd_addr, client_port))
 
-    sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-    sock.connect((bd_addr, port))
+client_sock,address = server_sock.accept()
+print ("Accepted connection from [{}]".format(address))
 
-    sock.send("hello!!")
+sock.send("hello!!")
 
-    sock.close()
+sock.close()
